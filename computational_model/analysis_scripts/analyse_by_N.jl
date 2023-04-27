cd9dc7dc596e16e488973f4bad9c956c049f3995
K.T. Jensen <ktj21@sagarmatha.eng.cam.ac.uk>
2023-04-27 14:37:54 +0100

    update analyses by model size

--- computational_model/analysis_scripts/analyse_by_N.jl
+++ computational_model/analysis_scripts/analyse_by_N.jl
@@ -1,81 +1,56 @@
+#in this script, we consider how rewards and rollout probabilities change over learning
+#for models of different sizes.
+
+#load some stuff
 include("anal_utils.jl")
 using ToPlanOrNotToPlan
-using NaNStatistics, MultivariateStats, Flux, PyCall, PyPlot, Random, Statistics
-using BSON: @save, @load
 
-loss_hp = LossHyperparameters(0, 0, 0, 0)
-Save = true
+println("analyzing learning and rollouts for different model sizes")
 
-greedy_actions = true
-no_planning = false
-seeds = 61:65
-Nhiddens = 40:20:120
-epochs = 0:100:2000
+loss_hp = LossHyperparameters(0, 0, 0, 0) #not computing losses
+Nhiddens = 40:20:120 #number of hidden units
+epochs = 0:50:1000 #training epochs to consider
 
-loaddir = "../models/maze/"
-resdir, figdir = "./results/", "../figs/maze/model/"
 
-meanrews, pfracs = [zeros(length(Nhiddens), length(seeds), length(epochs)) for _ = 1:2]
-pairwise_dists = zeros(length(Nhiddens), length(seeds), length(epochs), 16, 16)
+meanrews, pfracs = [zeros(length(Nhiddens), length(seeds), length(epochs)) for _ = 1:2] #containers for storing results
+for (ihid, Nhidden) = enumerate(Nhiddens) #for each network size
+    for (iseed, seed) = enumerate(seeds) #for each random seed
+        for (iepoch, epoch) = enumerate(epochs)
 
-for (ihid, Nhidden) = enumerate(Nhiddens)
-for (iseed, seed) = enumerate(seeds)
-for (iepoch, epoch) = enumerate(epochs)
+            filename = "N$(Nhidden)_T50_Lplan8_seed$(seed)_$epoch" #model to load
+            network, opt, store, hps, policy, prediction = recover_model(loaddir*filename) #load model parameters
+            
+            #instantiate environment and agent
+            Larena = hps["Larena"]
+            model_properties, wall_environment, model_eval = build_environment(
+                Larena, hps["Nhidden"], hps["T"], Lplan = hps["Lplan"], greedy_actions = greedy_actions
+            )
+            m = ModularModel(model_properties, network, policy, prediction, forward_modular)
 
-filename = "$loaddir/N$(Nhidden)_T50_seed$(seed)_Lplan8_$epoch"
-@load filename * "_hps.bson" hps
-@load filename * "_progress.bson" store
-@load filename * "_mod.bson" network
-@load filename * "_policy.bson" policy
-@load filename * "_prediction.bson" prediction
+            Random.seed!(1) #set random seed for reproducibility
+            batch_size = 5000 #number of environments to consider
+            tic = time()
+            #run the experiment
+            L, ys, rews, as, world_states, hs = run_episode(
+                m, wall_environment, loss_hp; batch=batch_size, calc_loss = false
+            )
+            plan_frac = sum(as .== 5)/sum(as .> 0.5) #fraction of actions that were rollouts
+            mean_rew = sum(rews .> 0.5) / batch_size #average reward per episode
 
-Larena = hps["Larena"]
-model_properties, wall_environment, model_eval = build_environment(
-    Larena, hps["Nhidden"], hps["T"], Lplan = hps["Lplan"], greedy_actions = greedy_actions
-)
-m = ModularModel(model_properties, network, policy, prediction, forward_modular)
+            println("N=$Nhidden, seed=$seed, epoch=$epoch, avg rew=$(mean_rew), rollout fraction=$(plan_frac)")
+            #store results
+            meanrews[ihid, iseed, iepoch] = mean_rew
+            pfracs[ihid, iseed, iepoch] = plan_frac
 
-Random.seed!(1)
-batch_size = 5000
-tic = time()
-L, ys, rews, as, world_states, hs = run_episode(
-    m, wall_environment, loss_hp; batch=batch_size, calc_loss = false
-)
-states = reduce((a, b) -> cat(a, b, dims = 3), [ws.agent_state for ws = world_states])
-wall_loc, ps = world_states[1].environment_state.wall_loc, world_states[1].environment_state.reward_location
-plan_frac = sum(as .== 5)/sum(as .> 0.5)
-mean_rew = sum(rews .> 0.5) / batch_size
-
-println(Nhidden, " ", seed, " ", epoch, " ", mean_rew, " ", plan_frac)
-
-meanrews[ihid, iseed, iepoch] = mean_rew
-pfracs[ihid, iseed, iepoch] = plan_frac
-
-### compute representational similarity ###
-Rmin, dt = 2, 2
-keep_inds = findall(sum(rews .> 0.5; dims=2)[:] .>= Rmin)
-all_hs, ps = hs[:, keep_inds, :], world_states[1].environment_state.reward_location
-mean_states = calc_mean_rep(dt, ps, keep_inds, rews, all_hs; trialnum=2)
-for i1 in 1:Larena^2
-    for i2 in 1:Larena^2
-        pairwise_dists[ihid, iseed, iepoch, i1, i2] = sqrt(
-            sum((mean_states[:, i1] - mean_states[:, i2]) .^ 2)
-        )
+        end
     end
 end
 
-
-end
-end
-end
-
+#save all results
 res_dict = Dict("seeds" => seeds,
                 "Nhiddens" => Nhiddens,
                 "epochs" => epochs,
                 "meanrews" => meanrews,
-                "planfracs" => pfracs,
-                "pairwise" => pairwise_dists)
-
-@save resdir * "rew_and_plan_by_n_new.bson" res_dict
-
+                "planfracs" => pfracs)
+@save datadir * "rew_and_plan_by_n_new.bson" res_dict
 
