f9079d1c552240af9964ab0a10c1e55186fe0297
K.T. Jensen <ktj21@sagarmatha.eng.cam.ac.uk>
2023-04-27 11:24:06 +0100

    change some filenames

---
+++ computational_model/analysis_scripts/run_all_analyses.jl
@@ -0,0 +1,18 @@
+# in this script, we call all of the model analysis functions.
+# this may take a while to run unless you have a very big computer
+
+include("analyse_human_data.jl")
+include("calc_human_prior.jl")
+
+include("analyse_rollout_timing.jl")
+include("repeat_human_actions.jl")
+include("perf_by_rollout_number.jl")
+include("compare_perf_without_rollout.jl")
+include("shuffle_rollout_times.jl")
+#include("behaviour_by_success.jl") (todo)
+include("model_replay_analyses.jl")
+#include("rollout_as_pg.jl") (todo)
+#include("analyse_by_N.jl")
+
+include("quantify_internal_model.jl")
+#include("analyse_hp_sweep.jl") $set run to false in sub scripts (todo)
